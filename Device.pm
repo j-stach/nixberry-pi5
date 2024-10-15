a3f9ba34a05b608c732c15c3d9ee7a52414f4291
j-stach <jordan.stachura@gmail.com>
2024-10-14 23:18:51 -0400

    updated

--- Device.pm
+++ Device.pm
@@ -2,13 +2,15 @@
 #/usr/bin/perl
 package Device;
 
+
 # Ensures the user-supplied device name is available for partitioning.
 sub ok {
+  my ($device) = @_;
   my @available = &available_devices;
-  foreach my $device (@available) {
-    if ($device eq $DEVICE) { return 1 }
+  foreach my $dev (@available) {
+    if ($dev eq $device) { return 1 }
   }
-  die "ERROR: Device '$DEVICE' is either mounted or nonexistant.";
+  die "ERROR: Device '$device' is either mounted or nonexistant.";
 }
 
 # Find unmounted devices that could be repartitioned
@@ -19,42 +21,62 @@ sub available_devices {
 }
 
 
-# Partition and format the device.
-sub partition_device {
+# Partition the device. 
+# The options are 2, 1, and 0 GB. 
+# If 0, no swap partition will be created.
+sub partition {
+  my ($device, $swap) = @_;
+
   # p1: Boot partition (FAT32)
+    # Boot size does not change, and so is placed first,
+    # to preserve the start sectors for both boot and root.
   # p2: Root partition (use all available space)
-  # p3: Swap partition (2GB Linux swap, type 82)
   my $commands = <<'COMMANDS';
 ,256M,0c,
-,+2G,82
 ,,,-
 COMMANDS
 
-  open(my $util, '|-', "sfdisk --wipe always $DEVICE") or die "$!";
-  print $util $commands;
-  close $util or die "ERROR: Partitioning failed: $!";
+  # p3: Swap partition (Linux swap, type 82)
+    # Swap is placed last because it can be moved 
+    # without disturbing the filesystem.
+  unless ($swap == 0) {
+    $commands = $commands."\n,+${swap}G,82\n";
+  }
 
-  # Format BOOT
-  system("mkfs.vfat -F 32 -n boot $DEVICE.p1") == 0 or die "$!";
-  # Format ROOT
-  my $ext4_options = "-L nixos -E lazy_itable_init=0,lazy_journal_init=0";
-  system("mkfs.ext4 $ext4_options -F $DEVICE.p2") == 0 or die "$!";
-  # Format SWAP
-  system("mkswap -L swap $DEVICE.p3 && swapon $DEVICE.p3") == 0 or die "$!";
+  # Wipe never lets us resize the image partitions in place.
+  open(my $tool, '|-', "sfdisk --wipe never $device") or die "$!";
+  print $tool $commands;
+  close $tool or die "$!";
 }
 
-
-sub mount_partitions {
-  # TODO Check path to make sure no other SDs are mounted.
-  my $mp = "/mnt/sd";
-  # Mount ROOT
-  make_path($mp);
-  system("mount $DEVICE.p2 $mp") == 0 or die "$!";
-  # Mount BOOT
-  make_path("$mp/boot");
-  system("mount $DEVICE.p1 $mp/boot") == 0 or die "$!";
-  system("sync") == 0 or die "$!";
-  return $mp;
-}
 
 1;
+
+
+## Make file system for newly-partitioned disk.
+#sub make_fs {
+#  my ($device) = @_;
+#  # Format BOOT
+#  system("mkfs.vfat -F 32 -n boot $device.p1") == 0 or die "$!";
+#  # Format ROOT
+#  my $ext4_options = "-L nixos -E lazy_itable_init=0,lazy_journal_init=0";
+#  system("mkfs.ext4 $ext4_options -F $device.p2") == 0 or die "$!";
+#  # Format SWAP
+#  system("mkswap -L swap $device.p3 && swapon $device.p3") == 0 or die "$!";
+#}
+
+
+##sub mount_partitions {
+#  # TODO Check path to make sure no other SDs are mounted.
+#  my $mp = "/mnt/sd";
+#  # Mount ROOT
+#  make_path($mp);
+#  system("mount $device.p2 $mp") == 0 or die "$!";
+#  # Mount BOOT
+#  make_path("$mp/boot");
+#  system("mount $device.p1 $mp/boot") == 0 or die "$!";
+#  system("sync") == 0 or die "$!";
+#  return $mp;
+#}
+
+
